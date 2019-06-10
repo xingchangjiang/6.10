183ec84eea13da3ec50c8e9d22ec145a4bbb2163
xingchangjiang <1547006143@qq.com>
2019-06-10 11:04:05 +0800

    'one'

---
+++ 6.9exam/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrap">
+    <button @click=add>点击</button>
+    <Dialog
+    v-show="flag"
+    :head="head"
+    :diacont="diacont"
+    :btn="btn"
+    ></Dialog>
+  </div>
+</template>
+<script>
+import Dialog from './components/dialog'
+export default {
+  props:{
+
+  },
+  components:{
+    Dialog
+  },
+  data(){
+    return {
+      head:'弹框+2',
+      diacont:'内容+2',
+      btn:["确定","取消","不知道"],
+      flag:false
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+    add(){
+      this.flag=!this.flag
+    }
+  },
+  created(){
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped lang="">
+
+</style>
---
+++ 6.9exam/src/components/dialog.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="mask">
+        <div class="content">
+            <header>{{head}}</header>
+            <div class="cont">{{diacont}}</div>
+            <button v-for="(item,index) in btn" :key="index">{{item}}</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:['head',"diacont",'btn'],
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped lang="">
+
+</style>
